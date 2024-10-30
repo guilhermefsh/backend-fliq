4043f1dd9ef182ff659fd7cf6c441ca3436b986f
Guilherme Ferreira <gferreiradasilva96@gmail.com>
2024-10-30 10:40:25 -0300

    Initial commit

---
+++ src/main/java/singleagency/project_fliq/ProjectFliqApplication.java
@@ -0,0 +1,13 @@
+package singleagency.project_fliq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFliqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFliqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/singleagency/project_fliq/ProjectFliqApplicationTests.java
@@ -0,0 +1,13 @@
+package singleagency.project_fliq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFliqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
